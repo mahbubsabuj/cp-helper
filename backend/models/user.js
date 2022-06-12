e22760ceb37dc39f72e50885c15e531b91d1d3e4
mahbubsabuj <mahbubsabuj3@gmail.com>
2022-06-13 01:34:51 +0600

    refactor: :recycle: userSchema

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 //name, email, city, country, passwordHash, onlineJudge handles(will be added), .......
 
 const userSchema = mongoose.Schema({
-  name: {type: String, required: true},
-  email: {type: String, required: true},
-  city: {type: String, required: true},
-  country: {type: String, required: true},
-  passwordHash: {type: String, required: true},
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  city: { type: String, default: "" },
+  country: { type: String, default: "" },
+  passwordHash: { type: String, required: true },
+  isAdmin: { type: Boolean, default: "" },
   //... and some other fields
 });
-
 
 exports.User = mongoose.model("User", userSchema);
