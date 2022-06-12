cbb1f55b38c3b479c4fa06f232eeb6de8a93e0ff
mahbubsabuj <mahbubsabuj3@gmail.com>
2022-06-13 01:25:57 +0600

    feat: :sparkles: Add more routes(get by id, put) on problems and tags route

--- backend/routes/problems.js
+++ backend/routes/problems.js
@@ -31,4 +31,49 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  if (!mongoose.isValidObjectId(id)) {
+    res.status(500).json({ success: false, message: "Invalid problem id!" });
+  } else {
+    const { name, difficulty, tags, judgeName, link } = req.body;
+    const prev = await Problem.findOne({ _id: id });
+    if (!prev) {
+      res.status(500).json({
+        success: false,
+        message: "Cannot update problem with given id!",
+      });
+    } else {
+      const problem = await Problem.findByIdAndUpdate(
+        id,
+        {
+          name: name || prev.name,
+          difficulty: difficulty || prev.difficulty,
+          tags: tags || prev.tags,
+          judgeName: judgeName || prev.judgeName,
+          link: link || prev.link,
+        },
+        { new: true }
+      );
+      if (!problem) {
+        res.status(500).json({
+          success: false,
+          message: "Cannot update problem with given id!",
+        });
+      } else {
+        res.status(201).json({ success: true, problem: problem });
+      }
+    }
+  }
+});
+
+router.get("/get/count", async (req, res) => {
+  const problemCount = await Problem.countDocuments();
+  if (!problemCount) {
+    res.status(404).json({ success: false, message: "" });
+  } else {
+    res.status(200).json({ success: true, problemCount: problemCount });
+  }
+});
+
 module.exports = router;
--- backend/routes/tags.js
+++ backend/routes/tags.js
@@ -3,6 +3,25 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const { Tag } = require("../models/tag");
 
+router.get("/", async (req, res) => {
+  const tagList = await Tag.find();
+  if (!tagList) {
+    res.status(500).json({ success: false, message: "" });
+  } else {
+    res.status(200).json({ success: true, tagList: tagList });
+  }
+});
+
+router.get("/:id", async(req, res) => {
+  const id = req.params.id;
+  const tag = await Tag.findById(id)
+  if (!tag) {
+    res.status(500).json({success: false, message: ""})
+  } else {
+    res.status(200).json({success: true, tag: tag})
+  }
+}) 
+
 router.post("/", async (req, res) => {
   const { name } = req.body;
   let tag = new Tag({ name });
@@ -13,6 +32,23 @@ router.post("/", async (req, res) => {
       .json({ success: false, message: "The tag cannot be created" });
   } else {
     res.status(200).json({ success: true, problem: tag });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  if (!mongoose.isValidObjectId(id)) {
+    res.status(500).json({ success: false, message: "Invalid tag id!" });
+  } else {
+    const tag = await Tag.findByIdAndDelete(id);
+    if (!tag) {
+      res.status(500).json({
+        success: false,
+        message: "tag with given id cannot be deleted!",
+      });
+    } else {
+      res.status(204).json({ success: true, message: "successfully deleted!" });
+    }
   }
 });
 
