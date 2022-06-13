0ccf6f0acb8a00d1aadf565d4cb8ac03c5ac8f6f
mahbubsabuj <mahbubsabuj3@gmail.com>
2022-06-13 11:32:48 +0600

    refactor: :recycle: problems, tags, users route

--- backend/routes/tags.js
+++ backend/routes/tags.js
@@ -6,50 +6,57 @@ const { Tag } = require("../models/tag");
 router.get("/", async (req, res) => {
   const tagList = await Tag.find();
   if (!tagList) {
-    res.status(500).json({ success: false, message: "" });
-  } else {
-    res.status(200).json({ success: true, tagList: tagList });
+    return res.status(500).json({ success: false, message: "" });
   }
+  return res.status(200).json({ success: true, tagList: tagList });
 });
 
-router.get("/:id", async(req, res) => {
+router.get("/:id", async (req, res) => {
   const id = req.params.id;
-  const tag = await Tag.findById(id)
+  const tag = await Tag.findById(id);
   if (!tag) {
-    res.status(500).json({success: false, message: ""})
-  } else {
-    res.status(200).json({success: true, tag: tag})
+    return res.status(500).json({ success: false, message: "" });
   }
-}) 
+  return res.status(200).json({ success: true, tag: tag });
+});
 
 router.post("/", async (req, res) => {
   const { name } = req.body;
   let tag = new Tag({ name });
   tag = await tag.save();
   if (!tag) {
-    res
+    return res
       .status(500)
       .json({ success: false, message: "The tag cannot be created" });
-  } else {
-    res.status(200).json({ success: true, problem: tag });
   }
+  return res.status(200).json({ success: true, problem: tag });
 });
 
 router.delete("/:id", async (req, res) => {
   const id = req.params.id;
   if (!mongoose.isValidObjectId(id)) {
-    res.status(500).json({ success: false, message: "Invalid tag id!" });
-  } else {
-    const tag = await Tag.findByIdAndDelete(id);
-    if (!tag) {
-      res.status(500).json({
-        success: false,
-        message: "tag with given id cannot be deleted!",
-      });
-    } else {
-      res.status(204).json({ success: true, message: "successfully deleted!" });
-    }
+    return res.status(500).json({ success: false, message: "Invalid tag id!" });
   }
+  const tag = await Tag.findByIdAndDelete(id);
+  if (!tag) {
+    return res.status(500).json({
+      success: false,
+      message: "tag with given id cannot be deleted!",
+    });
+  }
+  return res
+    .status(204)
+    .json({ success: true, message: "successfully deleted!" });
 });
+
+router.get("/", async (req, res) => {
+  const tagCount = await Tag.countDocuments();
+  if (!tagCount) {
+    return res.status(500).json({ success: false, message: "" });
+  }
+  return res.status(200).json({ success: true, tagCount: tagCount });
+});
+
+
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,24 +8,21 @@ const salt = process.env.SALT;
 router.get("/", async (req, res) => {
   const users = await User.find();
   if (!users) {
-    res.status(404).json({ success: false, message: "" });
-  } else {
-    res.status(200).json({ success: true, users: users });
+    return res.status(404).json({ success: false, message: "" });
   }
+  res.status(200).json({ success: true, users: users });
 });
 
 router.get("/:id", async (req, res) => {
   const id = req.params.id;
   if (!mongoose.isValidObjectId(id)) {
-    res.status(500).json({ success: false, message: "Invalid user id" });
-  } else {
-    const user = await User.findById(id);
-    if (!user) {
-      res.status(500).json({ success: false, message: "" });
-    } else {
-      res.status(200).json({ success: true, user: user });
-    }
+    return res.status(500).json({ success: false, message: "Invalid user id" });
   }
+  const user = await User.findById(id);
+  if (!user) {
+    return res.status(500).json({ success: false, message: "" });
+  }
+  return res.status(200).json({ success: true, user: user });
 });
 
 router.post("/", async (req, res) => {
@@ -40,59 +37,72 @@ router.post("/", async (req, res) => {
   });
   user = await user.save();
   if (!user) {
-    res
+    return res
       .status(500)
       .json({ success: false, message: "user cannot be created!" });
-  } else {
-    res.status(201).json({ success: true, user: user });
   }
+  return res.status(201).json({ success: true, user: user });
 });
 
 router.put("/:id", async (req, res) => {
   const id = req.params.id;
   if (!mongoose.isValidObjectId(id)) {
-    res.status(500).json({ success: false, mesasge: "Invalid user id!" });
-  } else {
-    const userPrev = await User.findById(id);
-    if (!userPrev) {
-      res.status(500).json({
-        success: false,
-        message: "the user cannot be updated!",
-      });
-    } else {
-      const { name, email, city, country, password, isAdmin } = req.body;
-      const user = await User.findByIdAndUpdate(
-        id,
-        {
-          name: name || userPrev.name,
-          email: email || userPrev.email,
-          city: city || userPrev.city,
-          country: country || userPrev.country,
-          passwordHash: password
-            ? bcrypt.hashSync(password, +salt)
-            : userPrev.passwordHash,
-          isAdmin: isAdmin || userPrev.isAdmin,
-        },
-        { new: true }
-      );
-      if (!user) {
-        res
-          .status(500)
-          .json({ success: false, message: "the user cannot be updated!" });
-      } else {
-        res.status(200).json({ success: true, user: user });
-      }
-    }
+    return res
+      .status(500)
+      .json({ success: false, mesasge: "Invalid user id!" });
   }
+  const userPrev = await User.findById(id);
+  if (!userPrev) {
+    return res.status(500).json({
+      success: false,
+      message: "the user cannot be updated!",
+    });
+  }
+  const { name, email, city, country, password, isAdmin } = req.body;
+  const user = await User.findByIdAndUpdate(
+    id,
+    {
+      name: name || userPrev.name,
+      email: email || userPrev.email,
+      city: city || userPrev.city,
+      country: country || userPrev.country,
+      passwordHash: password
+        ? bcrypt.hashSync(password, +salt)
+        : userPrev.passwordHash,
+      isAdmin: isAdmin || userPrev.isAdmin,
+    },
+    { new: true }
+  );
+  if (!user) {
+    return res
+      .status(500)
+      .json({ success: false, message: "the user cannot be updated!" });
+  }
+  return res.status(200).json({ success: true, user: user });
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(500).json({ success: false, message: "invalid user id" });
+  }
+  const user = await User.findByIdAndDelete(id);
+  if (!user) {
+    return res
+      .status(500)
+      .json({ success: false, message: "the user cannot be deleted!" });
+  }
+  return res
+    .status(200)
+    .json({ success: true, message: "user deleted successfully" });
 });
 
 router.get("/get/count", async (req, res) => {
   const userCount = await User.countDocuments();
   if (!userCount) {
-    res.status(500).json({ success: false, message: "" });
-  } else {
-    res.status(200).json({ success: true, userCount: userCount });
+    return res.status(500).json({ success: false, message: "" });
   }
+  return res.status(200).json({ success: true, userCount: userCount });
 });
 
 module.exports = router;
